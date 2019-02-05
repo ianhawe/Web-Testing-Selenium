94383e281a37476415222fe6563ef329af402baf
Ian <ianhawe24@hotmail.co.uk>
2019-02-05 23:08:34 +0000

    return values on last name, sex, years of experience added, profession also added however it only can get one value opposed to multiple values currently

--- Lib/selenium_class_walkthrough.rb
+++ Lib/selenium_class_walkthrough.rb
@@ -6,11 +6,11 @@ class QatoolsForm
   # Constants
   FIRST_NAME_FIELD_NAME = 'firstname'
   LAST_NAME_FIELD_NAME = 'lastname'
-  SEX_FIELD_NAME_FIELD_NAME = 'sex'
+  SEX_FIELD_NAME = 'sex'
   YEARS_OF_EXPERIENCE_FIELD_NAME = 'exp'
   # ADD DATE
   PROFESSION_FIELD_NAME = 'profession'
-  AUTOMATION_TOOL_FIELD_NAME = 'tool'
+  TOOL_FIELD_NAME = 'tool'
   def initialize
     # Link to driver through ruby chrome = interchangable for other browsers
     @chrome_driver = Selenium::WebDriver.for :chrome
@@ -29,19 +29,23 @@ class QatoolsForm
   end
 
   def input_sex_field(number)
-    @chrome_driver.find_elements(:name, SEX_FIELD_NAME_FIELD_NAME)[number].click
+    @chrome_driver.find_elements(:name, SEX_FIELD_NAME)[number].click
   end
 
   def input_years_of_experience_field(number)
     @chrome_driver.find_elements(:name, YEARS_OF_EXPERIENCE_FIELD_NAME)[number].click
   end
 
-  def input_profession_field(number)
-    @chrome_driver.find_elements(:name, PROFESSION_FIELD_NAME)[number].click
+  def input_profession_field(number0,number1)
+    @chrome_driver.find_elements(:name, PROFESSION_FIELD_NAME)[number0].click
+    @chrome_driver.find_elements(:name, PROFESSION_FIELD_NAME)[number1].click
   end
 
-  def input_automation_tool_field(number)
-    @chrome_driver.find_elements(:name, AUTOMATION_TOOL_FIELD_NAME)[number].click
+  def input_tool_field(number0, number1, number2)
+    @chrome_driver.find_elements(:name, TOOL_FIELD_NAME)[number0].click
+    @chrome_driver.find_elements(:name, TOOL_FIELD_NAME)[number1].click
+    @chrome_driver.find_elements(:name, TOOL_FIELD_NAME)[number2].click
+
   end
 
   def input_firstname_field_value
@@ -51,5 +55,20 @@ class QatoolsForm
   def current_url
     @chrome_driver.current_url
   end
-end
 
+  def input_lastname_field_value
+  @chrome_driver.find_element(:name, LAST_NAME_FIELD_NAME)['value']
+  end
+
+  def input_sex_field_value
+    @chrome_driver.find_element(:name, SEX_FIELD_NAME)['value']
+  end
+
+  def input_years_of_experience_field_value
+    @chrome_driver.find_element(:name, YEARS_OF_EXPERIENCE_FIELD_NAME)['value']
+  end
+
+  def input_profession_field_value
+    @chrome_driver.find_element(:name, PROFESSION_FIELD_NAME)['value']
+  end
+end
