1d5566c893df0c006c66de2a5b034204a2867bd2
Ian <ianhawe24@hotmail.co.uk>
2019-02-05 22:14:55 +0000

    Code fixed, errors removed

--- Lib/selenium_class_walkthrough.rb
+++ Lib/selenium_class_walkthrough.rb
@@ -1,9 +1,8 @@
 require 'selenium-webdriver'
-
+# This class will hold all the typical data form entry data
 class QatoolsForm
   # Page Object
   PRACTICE_FORM_URL = 'https://www.toolsqa.com/automation-practice-form'
-  # Constants
   FIRST_NAME_FIELD_NAME = 'firstname'
   LAST_NAME_FIELD_NAME = 'lastname'
 
@@ -11,20 +10,25 @@ class QatoolsForm
     # Link to driver through ruby chrome = interchangable for other browsers
     @chrome_driver = Selenium::WebDriver.for :chrome
   end
+
   def visit_practice_form
     @chrome_driver.get(PRACTICE_FORM_URL)
   end
+  
   # Passing values into the form
   def input_firstname_field(text)
     # This is to target a specific field on the webpage
     @chrome_driver.find_element(:name, FIRST_NAME_FIELD_NAME).send_keys(text)
   end
+ 
   def input_lastname_field(text)
     @chrome_driver.find_element(:name, LAST_NAME_FIELD_NAME).send_keys(text)
   end
+
   def input_firstname_field_value
     @chrome_driver.find_element(:name, FIRST_NAME_FIELD_NAME)['value']
   end
+
   def current_url
     @chrome_driver.current_url
   end
