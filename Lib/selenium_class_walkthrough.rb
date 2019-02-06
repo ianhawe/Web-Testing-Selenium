d21897fcb192c1074c5f4078deb6ca23a661226a
Ian <ianhawe24@hotmail.co.uk>
2019-02-06 09:23:58 +0000

    removed unrelated comments, and removed trailing white lines

--- Lib/selenium_class_walkthrough.rb
+++ Lib/selenium_class_walkthrough.rb
@@ -10,7 +10,6 @@ class QatoolsForm
   YEARS_OF_EXPERIENCE_FIELD_NAME = 'exp'
   PROFESSION_FIELD_NAME = 'profession'
   TOOL_FIELD_NAME = 'tool'
-  DATE_TEXT_FIELD_ID = 'datepicker'
 
   def initialize
     # Link to driver through ruby chrome = interchangable for other browsers
@@ -50,17 +49,12 @@ class QatoolsForm
     @chrome_driver.find_elements(:name, TOOL_FIELD_NAME)[number2].click
   end
 
-  def input_date_field(text)
-    @chrome_driver.find_element(:id, DATE_TEXT_FIELD_ID).send_keys(text)
-  end
-  
-  # This is where you get values from the form
-  def current_url
-    @chrome_driver.current_url
-  end
-
   def input_firstname_field_value
     @chrome_driver.find_element(:name, FIRST_NAME_FIELD_NAME)['value']
+  end
+
+  def current_url
+    @chrome_driver.current_url
   end
 
   def input_lastname_field_value
@@ -82,13 +76,12 @@ class QatoolsForm
   def input_tool_field_value
     @chrome_driver.find_element(:name, TOOL_FIELD_NAME)['value']
   end
-
-  def input_date_field_value
-    @chrome_driver.find_element(:id, DATE_TEXT_FIELD_ID)['value']
-  end
 end
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of dcbb3b7... added to the date field form, also added a method to get the value of date
 # Testing Area
 test = QatoolsForm.new
 test.visit_practice_form
@@ -98,7 +91,10 @@ test.input_sex_field(1)
 test.input_years_of_experience_field(0)
 test.input_profession_field(0, 1)
 test.input_tool_field(0, 1, 2)
+<<<<<<< HEAD
 test.input_date_field('24/12/1991')
+=======
+>>>>>>> parent of dcbb3b7... added to the date field form, also added a method to get the value of date
 
 p test.input_firstname_field_value
 p test.input_lastname_field_value
@@ -106,5 +102,9 @@ p test.input_sex_field_value
 p test.input_years_of_experience_field_value
 p test.input_profession_field_value
 p test.input_tool_field_value
+<<<<<<< HEAD
 p test.input_date_field_value
 sleep 15
+=======
+sleep 2
+>>>>>>> parent of dcbb3b7... added to the date field form, also added a method to get the value of date
